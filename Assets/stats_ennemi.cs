d4a9b760bb3d8287a1f4ab463353992c4d2c628f
AnataarXVI <105481566+AnataarXVI@users.noreply.github.com>
2022-06-20 23:23:25 +0200

    Ennemi Movement
    Add ennemi mouvement

---
+++ Assets/stats_ennemi.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stats_ennemi : MonoBehaviour
+{
+    // Initialisation des stats de l'ennemi
+    public int pv = 100;
+    public int damage = 15;
+
+}
